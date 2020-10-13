3ede527e0941c2df1ec85c5a7b9a04d8b7a24c86
fkdmkhr <mf.bankai.nov05@gmail.com>
2020-10-14 03:02:26 +0900

    DB接続の追加

---
+++ ToDoList/Pages/Find.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Data;
+using ToDoList.Models;
+
+namespace ToDoList.Pages
+{
+    public class FindModel : PageModel
+    {
+        private readonly ToDoListContext _context;
+
+        public IList<Person> People { get; set; }
+
+
+        public void OnGet()
+        {
+        }
+    }
+}
--- ToDoList/Pages/Index.cshtml.cs
+++ ToDoList/Pages/Index.cshtml.cs
@@ -5,21 +5,52 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using ToDoList.Data;
+using ToDoList.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly ToDoListContext _context;
+
+        public IndexModel(ToDoListContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public IList<Person> Person { get; set; }
 
+        public async Task OnGetAsync()
+        {
+            Person = await _context.Person.ToListAsync();
         }
+
+        //public string Message = "no message";
+
+        //[DataType(DataType.Text)]
+        //public string Name { get; set; }
+
+        //[DataType(DataType.Text)]
+        //public string Password { get; set; }
+
+        //[DataType(DataType.Text)]
+        //public string Mail { get; set; }
+
+        //[DataType(DataType.Text)]
+        //public string Tel { get; set; }
+
+        //public void OnGet()
+        //{
+        //    Message = "入力してください。";
+        //}
+
+        //public void OnPost(string name, string password, string mail, string tel)
+        //{
+        //    Message = "[Name: " + name + ", password ( " + password.Length + " chars), mail" + mail + " <" + tel + ">]";
+        //}
     }
 }
